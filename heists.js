4b2df6399352d84315465e4b53bc11f17e8fd389
MistehTimmeh <corybenrae@gmail.com>
2023-12-26 16:59:22 -0600

    adding files
    oijergpaijrgpiugpaioufjawef

---
+++ heists.js
@@ -0,0 +1,113 @@
+/*
+Heist Info Template for Future Heists
+
+,
+	{
+		"name": "",
+		"levelSet": "",
+		"gameplayStyle": "",
+		"recommendedStealthFavours": "",
+		"recommendedLoudFavours": "",
+	}
+*/
+fetch('heists.json')
+	.then(response => response.json())
+	.then(json => console.log(json));
+
+let oldNumber = -1;
+
+
+function regen(heistNumber) {
+	let parent = document.getElementById('fade-box');
+	
+	let randomBox = document.createElement('div');
+	randomBox.id = 'random-box';
+	randomBox.className = 'center center-div';
+	
+	let heistNameP = document.createElement('p');
+	heistNameP.id = 'heist-name';
+	heistNameP.className = 'fade-in-anim';
+	
+	let infoBoxLevelSet = document.createElement('div');
+	infoBoxLevelSet.className = 'info-box';
+	
+	let levelSetHeadingP = document.createElement('p');
+	levelSetHeadingP.className = 'heading';
+	levelSetHeadingP.textContent = 'RESPECTIVE LEVEL SET';
+	
+	let levelSetValueP = document.createElement('p');
+	levelSetValueP.className = 'fade-in-anim';
+	levelSetValueP.id = 'level-set';
+	
+	let infoBoxGameplayStyle = document.createElement('div');
+	infoBoxGameplayStyle.className = 'info-box';
+	
+	let gameplayStyleHeadingP = document.createElement('p');
+	gameplayStyleHeadingP.className = 'heading';
+	gameplayStyleHeadingP.textContent = 'GAMEPLAY STYLE';
+	
+	let gameplayStyleValueP = document.createElement('p');
+	gameplayStyleValueP.className = 'fade-in-anim';
+	gameplayStyleValueP.id = 'gameplay-style';
+	
+	let infoBoxFavourInfo = document.createElement('div');
+	infoBoxFavourInfo.className = 'favour-info';
+	
+	let favourInfoHeadingP = document.createElement('p');
+	favourInfoHeadingP.className = 'heading';
+	favourInfoHeadingP.textContent = 'RECOMMENDED FAVOURS';
+	
+	let stealthFavoursP = document.createElement('p');
+	stealthFavoursP.className = 'fade-in-anim';
+	let loudFavoursP = document.createElement('p');
+	loudFavoursP.className = 'fade-in-anim';
+	
+	let heistInfo = heists[heistNumber];
+	
+	oldNumber = heistNumber;
+	
+	heistNameP.textContent = heistInfo['name'];
+	levelSetValueP.textContent = heistInfo['levelSet'];
+	gameplayStyleValueP.textContent = heistInfo['gameplayStyle'];
+	stealthFavoursP.textContent = "Stealth: " + heistInfo['recommendedStealthFavours'];
+	loudFavoursP.textContent = "Loud: " + heistInfo['recommendedLoudFavours'];
+	
+	parent.innerHTML = '';
+	
+	infoBoxLevelSet.appendChild(levelSetHeadingP);
+	infoBoxLevelSet.appendChild(levelSetValueP);
+	
+	infoBoxGameplayStyle.appendChild(gameplayStyleHeadingP);
+	infoBoxGameplayStyle.appendChild(gameplayStyleValueP);
+	
+	infoBoxFavourInfo.appendChild(favourInfoHeadingP);
+	infoBoxFavourInfo.appendChild(stealthFavoursP);
+	infoBoxFavourInfo.appendChild(loudFavoursP);
+	
+	randomBox.appendChild(heistNameP);
+	randomBox.appendChild(infoBoxLevelSet);
+	randomBox.appendChild(infoBoxGameplayStyle);
+	randomBox.appendChild(infoBoxFavourInfo);
+	
+	parent.appendChild(randomBox);
+	
+	
+}
+
+function generateRandomHeist() {
+	let heistNumber = Math.floor(Math.random() * (heists.length - 1));
+	
+	while(oldNumber == heistNumber) {
+		heistNumber = Math.floor(Math.random() * (heists.length - 1));
+	}
+	
+	regen(heistNumber);
+}
+
+function load()
+{
+	document.getElementById('new-heist-button').addEventListener("click", generateRandomHeist);
+	generateRandomHeist();
+}
+
+document.addEventListener("DOMContentLoaded", load); 
